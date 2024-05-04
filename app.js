600f2ad863a1d2e45fa29d8685fd918967aa81ec
Henrique Eduardo Souza <h.eduardo.souza@avanade.com>
2024-05-04 15:01:54 -0300

    Ajustes de CORS

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
